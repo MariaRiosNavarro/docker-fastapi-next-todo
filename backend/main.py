cef709ef102d64869f59c65b4c9d561f7ce5b5b2
Maria Rios Navarro <propscode254@gmail.com>
2024-09-08 16:38:08 +0200

    add new adress

--- backend/main.py
+++ backend/main.py
@@ -53,7 +53,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["http://localhost:3000", "https://docker-fastapi-next-todo-1.onrender.com"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
